2252992354c4bb47f58c94a94dc1cf71567e1522
Florin Matei <contact@florinmatei.com>
2020-03-23 16:12:51 +0200

    Show all errors

--- db.php
+++ db.php
@@ -1,4 +1,13 @@
 <?php
+
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+
+
 $db= new Mysqli;
 $db -> connect('localhost', 'root', '', 'crud');
 if(!$db){
