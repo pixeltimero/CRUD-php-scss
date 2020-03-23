19914e01fc66b61361714339a13fcda17c989c7e
Florin Matei <contact@florinmatei.com>
2020-03-23 13:32:54 +0200

    Update file extensions

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>PHP Crud</title>
+<link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+<div class="container">
+  <div class="header">
+    <h1>Todo list</h1>
+    <a href="#">Add new task</a>
+  </div>
+  <div class="content">
+    <div class="item">
+      <div class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, libero nemo. Praesentium dolor in, optio iste accusantium aliquid cumque harum quod minima qui atque! Perferendis iusto nulla odit cupiditate natus!</div>
+      <div class="actions">
+        <a href="#" class="icon"><i class="icon"></i></a>
+        <a href="#" class="icon"><i class="icon"></i></a>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
