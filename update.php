d773bf0fc944d01b6dab408098b54c2bfe4449d9
Florin Matei <contact@florinmatei.com>
2020-03-24 21:37:52 +0200

    Update - fixes

--- update.php
+++ update.php
@@ -9,27 +9,33 @@ include ('db.php');
 <body>
 <div class="container">
     <div class="header">
-        <h1>Add new task</h1>
+        <h1>Update task</h1>
         <a href="index.php">View all tasks</a>
     </div>
     <div class="content">
-        <form action="create.php" method="POST">
+        <form action="<?=$_SERVER['PHP_SELF'];?>" method="POST">
             <div class="item single">
                 <div class="text">
                     <?php 
-                    if (isset($_GET['id'])) {
-                        $id=$_GET['id'];
-                        $sql = "select * from tasks there id = '$id'";
-                        $rows = $db->query($sql);
-                       
-                    }
-                    while($row = $rows->fetch_assoc()){
-                        ?>
-                        <textarea placeholder="Type something here..." name="text" required><?php echo $row['text']; ?></textarea>
-                        <?php } ?>
+                        if (isset($_GET['id'])) {
+                            $id=$_GET['id'];
+                            $sql = "select * from tasks where id =$id";
+                            $rows = $db->query($sql) or die($db->error);
+                            $row = $rows->fetch_assoc();
+                            echo ' <textarea placeholder="Type something here..." name="text" required>'.$row['text'].'</textarea>';
+                            echo '<input type="text" name="id" value="'.$row['id'].'">';
+                        } 
+                        if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+                            $text=$_POST['text'];
+                            $id2=$_POST['id'];
+                            $sql2 = "update tasks set text='$text' where id =$id2";
+                            $db->query($sql2) or die($db->error);
+                            header('location:index.php');
+                        }
+                    ?>
                 </div>
                 <div class="actions">
-                    <button class="button" type="submit"><i class="icon icon-icon-check"></i>Create task</button>
+                    <button class="button" type="submit"><i class="icon icon-icon-check"></i>Update task</button>
                     <a href="index.php" class="button white"><i class="icon icon-icon-cancel"></i>Cancel</a>
                 </div>
             </div>
